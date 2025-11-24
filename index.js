b84b53807f1e88b523f152b8df96570a90bb2807
shrabonmolla <shrabonmolla007@gmail.com>
2025-11-25 01:02:03 +0600

    payment related apis implemented

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.visb7wb.mongodb.net/?appName=Cluster0`;
 
@@ -30,6 +30,14 @@ async function run() {
   try {
     const db = client.db("parcelsDb");
     const parcelCol = db.collection("parcelCol");
+    // get parcel by id
+    app.get("/payments/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const cursor = parcelCol.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     // get all parcel
     app.get("/parcels", async (req, res) => {
@@ -57,6 +65,36 @@ async function run() {
       const query = { _id: new ObjectId(id) };
       const result = await parcelCol.deleteOne(query);
       res.send(result);
+    });
+
+    // payment related apis
+    app.post("/create-checkout-session", async (req, res) => {
+      const paymentInfo = req.body;
+      const amount = parseInt(paymentInfo.cost) * 100;
+
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            price_data: {
+              currency: "usd",
+              unit_amount: amount,
+              product_data: {
+                name: `Please pay for: ${paymentInfo.parcelName}`,
+              },
+            },
+            quantity: 1,
+          },
+        ],
+        metadata: {
+          parcelId: paymentInfo.parcelId,
+        },
+        customer_email: paymentInfo.senderEmail,
+        mode: "payment",
+        success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success`,
+        cancel_url: `${process.env.SITE_DOMAIN}/dashboard/payment-cancel`,
+      });
+      console.log(session);
+      res.send({ url: session.url });
     });
 
     await client.db("admin").command({ ping: 1 });
